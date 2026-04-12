b6df54637effa4aba5acfa6b9ef02b4e16824768
ma-muktadeer <muktadeer.ma@gmail.com>
2026-04-12 13:54:08 +0600

    ok

--- src/app/career-grid/career-grid.ts
+++ src/app/career-grid/career-grid.ts
@@ -9,16 +9,16 @@ import { Component } from '@angular/core';
 export class CareerGrid {
   // features.component.ts
   features = [
-    { title: 'Monthly Application Limit', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'Application Boosting', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'Direct Message to Employer', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'Application Insights', size: 'wide', desc: 'With Premium, apply for unlimited jobs and explore diverse paths...' }, // Boro width
-    { title: 'Upload Video CV', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'See Matching Percentage', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'See who Viewed my Profile', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'View who Shortlisted me', size: 'small', desc: 'With Premium, apply...' },
-    { title: 'Track Hiring Activities on Jobs', size: 'tall', desc: 'With Premium, apply...' }, // Boro height
-    { title: 'See Job Details after Deadline', size: 'tall', desc: 'With Premium, apply...' }, // Boro height
+    { title: 'Monthly Application Limit', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'Application Boosting', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'Direct Message to Employer', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'Application Insights', size: 'wide', desc: 'With Premium, apply for unlimited jobs and explore diverse paths...', img: 'p1.png' }, // Boro width
+    { title: 'Upload Video CV', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'See Matching Percentage', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'See who Viewed my Profile', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'View who Shortlisted me', size: 'small', desc: 'With Premium, apply...', img: 'p1.png' },
+    { title: 'Track Hiring Activities on Jobs', size: 'tall', desc: 'With Premium, apply...', img: 'p1.png' }, // Boro height
+    { title: 'See Job Details after Deadline', size: 'tall', desc: 'With Premium, apply...', img: 'p1.png' }, // Boro height
   ];
 }
 
